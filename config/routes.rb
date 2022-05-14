6947abee1b5edd76d008b7f87ef99f34e85961a2
Jed-Tapiador <doom.slaver2@gmail.com>
2022-05-14 15:35:38 +0800

    root updated, sign up and log in pages done. Header revision

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   resources :posts, only: [:index]
   resources :friends, only: [:index]
 
-  root "posts#index"
+  # root "posts#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+  
 end
