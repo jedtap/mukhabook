ade954fb375247640831505187d60e3a7de887c6
Jed-Tapiador <doom.slaver2@gmail.com>
2022-05-20 20:58:26 +0800

    header revision, forget PW styling, protected pages, find a friend new logic

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @profiles = Profile.all.shuffle
+    @profiles = Profile.where.not(id: current_user.profile.id).shuffle
   end
 end
