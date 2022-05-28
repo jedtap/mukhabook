eadfdd76ad90a3d216129bdca14595feb06f7256
Jed-Tapiador <doom.slaver2@gmail.com>
2022-05-28 12:27:17 +0800

    complete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(body: params[:body], profile_id: params[:profile_id], post_id: params[:post_id] ).save
-    redirect_back(fallback_location:"/")
+    # redirect_back(fallback_location:"/")
+    redirect_to url:"/posts##{params[:post_id]}"
   end
 
 end
