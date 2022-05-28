6aed309f77c277a38589d90c5532ff7e2cbc2221
Jed-Tapiador <doom.slaver2@gmail.com>
2022-05-28 11:34:34 +0800

    add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = current_user.profile.posts.first.comments.build(comment_params)
-    @comment.save
+    @comment = Comment.new(body: params[:body], profile_id: params[:profile_id], post_id: params[:post_id] ).save
     redirect_back(fallback_location:"/")
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:profile_id, :post_id, :body)
   end
 
 end
