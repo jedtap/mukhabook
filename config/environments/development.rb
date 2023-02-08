9071168a3ac5e55bb9c6fb44a39e47985a93ff1e
jedtap <doom.slaver2@gmail.com>
2023-02-08 19:29:39 +0800

    edits for deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   config.server_timing = true
 
-  config.hosts << "rails-production-049e.up.railway.app"
+  config.hosts << "rails-production-b6b8.up.railway.app"
 
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
